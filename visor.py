dd30c40b6d91c0bafd52648b27f0d4ec588deb9c
TheCyberAwarePotatoMan <87954311+TheCyberAwarePotatoMan@users.noreply.github.com>
2025-12-26 22:07:08 -0600

    Replace neopixel with Pi5Neo for LED handling
    Refactor to use Pi5Neo for LED control and remove fake classes.

--- visor.py
+++ visor.py
@@ -37,36 +37,13 @@ noses= {
      }
 
 import warnings
+from pi5neo import Pi5Neo
 
-try:
-    import neopixel as np
-    import board as bd
-except ImportError:
-    warnings.warn("\033[93m NeoPixels library or board library couldn't be imported — Emulation mode active \033[0m")
+eye_starting_led = 0
+mouth_starting_led = eye_starting_led + eye_len
+nose_starting_led = mouth_starting_led + mouth_len
 
-    # Fake NeoPixel class
-    class FakeNeoPixelStrip(list):
-        def __init__(self, pin, length):
-            super().__init__([(0, 0, 0)] * length)
-        def show(self):
-            pass  # does nothing
 
-    # Fake module to mimic 'neopixel'
-    class FakeNeoPixelModule:
-        NeoPixel = FakeNeoPixelStrip
-
-    # Fake board pins
-    class FakeBoard:
-        D1 = "D1"
-        D2 = "D2"
-        D3 = "D3"
-
-    np = FakeNeoPixelModule
-    bd = FakeBoard
-
-mouth_pin = bd.D1
-eye_pin = bd.D2
-nose_pin = bd.D3
 
 def emotion_len_checker(prin=False): #checks to ensure the emotions are the correct length for there led string
     ok = True
@@ -92,14 +69,15 @@ def emotion_len_checker(prin=False): #checks to ensure the emotions are the corr
 def load_class():
     class ProtogenFaceRenderer:
             def __init__(self):
-                self.mouth=np.NeoPixel(mouth_pin, mouth_len)
-                self.eye=np.NeoPixel(eye_pin, eye_len)
-                self.nose=np.NeoPixel(nose_pin, nose_len)
+                self.neo = Pi5Neo('/dev/spidev0.0', (eye_len + mouth_len + nose_len), 800)
 
-            def _render(self, strip, mask, color): #loops over the LED's checks if the should be lit and if so sets them to the provided color
+            def _render(self, starting_led, mask, color): #loops over the LED's checks if the should be lit and if so sets them to the provided color
                 for i, active in enumerate(mask):
-                    strip[i] = color if active else (0, 0, 0)
-                strip.show()
+                    self.neo.set_led_color((i + starting_led),
+                                            color[0] if active else 0,
+                                            color[1] if active else 0,
+                                            color[2] if active else 0)
+                self.neo.update_strip()
 
             def render_mouth(self, Red, Green, Blue, Emotion='normal'):
                 """Renders mouth LED's bassed on emotion mask
@@ -109,7 +87,7 @@ def load_class():
                     Green (int): Green channel (0-255)
                     Blue (int): Blue channel (0-255)
                     Emotion (str): Emotion key from the 'mouths' dictionary. (use list_mouth_emotions() to get a list of emotions)"""
-                self._render(self.mouth, mouths[Emotion], (Red, Green, Blue))
+                self._render(mouth_starting_led, mouths[Emotion], (Red, Green, Blue))
             
             def render_eye(self, Red, Green, Blue, Emotion='normal'):
                 """Renders eye LED's bassed on emotion mask
@@ -119,7 +97,7 @@ def load_class():
                     Green (int): Green channel (0-255)
                     Blue (int): Blue channel (0-255)
                     Emotion (str): Emotion key from the 'eyes' dictionary. (use list_eye_emotions() to get a list of emotions)"""
-                self._render(self.eye, eyes[Emotion], (Red, Green, Blue))
+                self._render(eye_starting_led, eyes[Emotion], (Red, Green, Blue))
             
             def render_nose(self, Red, Green, Blue, Emotion='normal'):
                 """Renders nose LED's bassed on emotion mask
@@ -129,7 +107,7 @@ def load_class():
                     Green (int): Green channel (0-255)
                     Blue (int): Blue channel (0-255)
                     Emotion (str): Emotion key from the 'noses' dictionary. (use list_nose_emotions() to get a list of emotions)"""
-                self._render(self.nose, noses[Emotion], (Red, Green, Blue))
+                self._render(nose_starting_led, noses[Emotion], (Red, Green, Blue))
             
             def render_face(self, Red, Green, Blue, Emotion='normal'):
                 """Renders all LED's bassed on emotion mask
@@ -139,9 +117,9 @@ def load_class():
                     Green (int): Green channel (0-255)
                     Blue (int): Blue channel (0-255)
                     Emotion (str): Emotion key from the dictionary's."""
-                self._render(self.mouth, mouths[Emotion], (Red, Green, Blue))
-                self._render(self.eye, eyes[Emotion], (Red, Green, Blue))
-                self._render(self.nose, noses[Emotion], (Red, Green, Blue))
+                self._render(mouth_starting_led, mouths[Emotion], (Red, Green, Blue))
+                self._render(eye_starting_led, eyes[Emotion], (Red, Green, Blue))
+                self._render(nose_starting_led, noses[Emotion], (Red, Green, Blue))
             
             def list_mouth_emotions(self): #lists the emotions
                 """Returns list of mouth emotions"""
